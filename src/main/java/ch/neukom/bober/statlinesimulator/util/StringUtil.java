3ba3ee72ad46bf29fcd2ec2e024afe9be7d82baf
Luca Neukom <luca.neukom@gmail.com>
2024-10-28 23:39:51 +0100

    add StringUtil#withFirstLetterUpperCase

--- src/main/java/ch/neukom/bober/statlinesimulator/util/StringUtil.java
+++ src/main/java/ch/neukom/bober/statlinesimulator/util/StringUtil.java
@@ -8,4 +8,11 @@ public class StringUtil {
     public static boolean isNotNullOrEmpty(String string) {
         return !Strings.isNullOrEmpty(string);
     }
+
+    public static String withFirstLetterUpperCase(String string) {
+        if (string.length() <= 1) {
+            return string.toUpperCase();
+        }
+        return Character.toUpperCase(string.charAt(0)) + string.substring(1).toLowerCase();
+    }
 }
